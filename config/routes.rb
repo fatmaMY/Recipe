7c63ea2d8c6bc4902d0edd7cd7c0649931c5dcbe
fatmaMY <fatima.mohammad736@gmail.com>
2017-04-23 13:24:47 +0300

    add routes to secrets and to about

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :food_recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+  get 'about', to: 'application#about' 
+  get 'secrets', to: 'application#secrets'
+
 end
