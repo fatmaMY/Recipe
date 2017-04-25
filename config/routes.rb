d679615fb581b2ef8b3a71515255c0e602233ea3
fatmaMY <fatima.mohammad736@gmail.com>
2017-04-25 03:06:50 +0300

    adding nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+		resources :categories, :food_recipes
+	end
+
+	get 'admin', to: 'admin#control_panel'
+
+	root 'application#home'
+	get 'about', to:'application#about'
+	get 'secrets', to: 'application#secrets'
+	# get 'admin', to: 'admin#control_panel'
   resources :users
   resources :categories
   resources :food_recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
-  get 'about', to: 'application#about' 
-  get 'secrets', to: 'application#secrets'
+ 
 
 end
