0d3bd1d2de57d929fd5601d8af795d52ef10ddd9
fatmaMY <fatima.mohammad736@gmail.com>
2017-04-26 14:45:22 +0300

    modifying categories rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	# delete 'admin/categories/:id', to: 'admin/categories#destroy', as: 'destroy_admin_category'
   namespace :admin do
 		resources :categories, :food_recipes
 	end
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
 	get 'secrets', to: 'application#secrets'
 	# get 'admin', to: 'admin#control_panel'
   resources :users
-  resources :categories
+  resources :categories, except: [:destroy]
   resources :food_recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
